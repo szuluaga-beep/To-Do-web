5640d26ba57aa1d5252a33976edce1ebc68b7cdb
Juanchos2905 <juanchos2905@gmail.com>
2023-08-10 17:27:40 -0500

    v1

---
+++ script/script.js
@@ -0,0 +1,20 @@
+/*TIPS: *No olvides utilizar el almacenamiento local (localStorage) para que las tareas queden guardadas en caso
+ de que la aplicación se cierre.*/
+
+//El sistema debe permitir EDITAR o MODIFICAR una tarea.
+
+//El sistema debe permitir ELIMINAR una tarea.
+
+//El sistema debe permitir AGREGAR una o varias tareas tarea.
+
+//El sistema deber permitir MARCAR una tarea como completada
+
+//El sistema debe permitir dar diferentes PRIORIDADES a las tareas
+//EJEMPLO:
+
+//Sacar la basura - Prioridad: media
+
+//El sistema debe permitir visualizar tareas por CATEGORÍAS o ETIQUETAS
+//EJEMPLO:
+
+/*Categorías disponibles: CASA, TRABAJO, EMPRENDIMIENTO E.T.C */
