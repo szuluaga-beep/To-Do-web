01d855afbf2a73542607451fa59309042af04f2b
szuluaga-beep <steven000069@hotmail.com>
2023-09-04 08:41:42 -0500

    Trabajando con localstorage

--- script/script.js
+++ script/script.js
@@ -1,6 +1,43 @@
 /*TIPS: *No olvides utilizar el almacenamiento local (localStorage)
  para que las tareas queden guardadas en caso
  de que la aplicación se cierre.*/
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+
+function crearTarea(nombreTarea) {
+  let objetoTarea = {
+    thing: nombreTarea,
+    checked: false,
+    priority: "Alta",
+    category: "Casa",
+  };
+
+  itemsArray.push(objetoTarea);
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
+  // console.log(itemsArray)
+}
+
+function borrarTarea(posicion) {
+  itemsArray.splice(posicion, 1);
+  // itemsArray = itemsArray.filter((tarea, i) => i !== posicion && tarea);
+
+  localStorage.setItem("items", JSON.stringify(itemsArray));
+  location.reload();
+}
+
+function actualizarTarea(tarea, posicion) {
+  itemsArray[posicion].thing= tarea
+  localStorage.setItem("items", JSON.stringify(itemsArray))
+  location.reload()
+}
+function countPend() {
+  const contadorPendientes = itemsArray.filter(
+    (tarea) => tarea.checked === false
+  );
+
+  return contadorPendientes.length;
+}
+
 function displayFooter() {
   let page = `      
      
@@ -21,29 +58,189 @@ function displayFooter() {
         </ul>
         <button onclick="borrarCompletados()" id="clear-completed" class="clear-completed">Borrar completados</button>
       </footer>
-    `
-  document.querySelector('.footer').innerHTML = page
+    `;
+  document.querySelector(".footer").innerHTML = page;
 }
 
 // Codigo DOM #1
-document.querySelector('.new-todo').addEventListener('keyup', (event) => {
+document.querySelector(".new-todo").addEventListener("keyup", (event) => {
   if (
     event.keyCode === 13 &&
-    document.querySelector('.new-todo').value.length > 0
+    document.querySelector(".new-todo").value.length > 0
   ) {
-    const item = document.querySelector('.new-todo')
+    const item = document.querySelector(".new-todo");
     //Llamar la función que crea la tarea.**
+    // console.log(item.value);
+    crearTarea(item.value);
   }
-})
+});
 // Codigo DOM #2
+// este fragmento permite conservar el estado del checkbox (true o false) en el localStorage
 
+function activateCheckboxListeners() {
+  const checkboxes = document.querySelectorAll(".toggle");
+  checkboxes.forEach((ch, i) => {
+    ch.addEventListener("click", () => {
+      itemsArray[i].checked = ch.checked;
+      localStorage.setItem("items", JSON.stringify(itemsArray));
+      location.reload();
+    });
+  });
+}
 // Codigo DOM #3
+// Permite que la acción eliminar impacte el DOM del HTML, acá debes agegar algoritmo de eliminar tarea
 
+function activateDeleteListeners() {
+  let deleteBtn = document.querySelectorAll(".deleteBtn");
+  deleteBtn.forEach((db, i) => {
+    db.addEventListener("click", () => {
+      //Llamar la función que elimina la tarea
+      borrarTarea(i);
+    });
+  });
+}
 // Codigo DOM #4
 
-// Codigo DOM #5
+// Permite que la acción editar de las 2 listas desplegables "prioridad" y "categoría" impacte el DOM del HTML cuando cambies de opción, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
 
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll(".editBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  const prioritySelects = document.querySelectorAll(
+    ".edit-controller select"
+  )[0];
+  const categorySelects = document.querySelectorAll(
+    ".edit-controller select"
+  )[1];
+
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      updateController[i].style.display = "block";
+      inputs[i].disabled = false;
+      inputs[i].style.background = '#fff';
+      inputs[i].style.padding = '5px';
+
+      prioritySelects.value = itemsArray[i].priority;
+      categorySelects.value = itemsArray[i].category;
+    });
+  });
+
+  document.querySelectorAll("#priority").forEach((select, i) => {
+    select.addEventListener("change", (event) => {
+      // console.log(event.target.value)
+      itemsArray[i].priority = event.target.value;
+      localStorage.setItem("items", JSON.stringify(itemsArray));
+      location.reload();
+    });
+  });
+  document.querySelectorAll("#category").forEach((select, i) => {
+    select.addEventListener("change", (event) => {
+      // console.log(event.target.value)
+      itemsArray[i].category = event.target.value;
+      localStorage.setItem("items", JSON.stringify(itemsArray));
+      location.reload();
+    });
+  });
+}
+// Codigo DOM #5
+// Permite que la acción guardar el nuevo nombre de la tarea cuando decides editar y que impacte el DOM del HTML, acá debes agegar algoritmo de actualizar tarea
+
+function activateSaveListeners() {
+  const saveBtn = document.querySelectorAll(".saveBtn");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  saveBtn.forEach((sB, i) => {
+    sB.addEventListener("click", () => {
+      // Llamar la función que guarda la actualización la tarea
+      // console.log(inputs[i].value)
+      actualizarTarea(inputs[i].value, i);
+    });
+  });
+}
 // Codigo DOM #6
+// Esta es la lógica para el botón "cancelar" cuando presionas editar una tarea, inserta este código tal cual, el reto está en saber en qué parte de tu código debes usarlo.
+
+function activateCancelListeners() {
+  const cancelBtn = document.querySelectorAll(".cancelBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  cancelBtn.forEach((cB, i) => {
+    cB.addEventListener("click", () => {
+      updateController[i].style.display = "none";
+      inputs[i].disabled = true;
+      inputs[i].style.border = "none";
+      location.reload()
+    });
+  });
+}
+
+function displayItems() {
+  let items = "";
+  for (let i = 0; i < itemsArray.length; i++) {
+    items += `    <div class="item">
+                    <div class="input-controller">
+                      <input class="toggle" type="checkbox" id="check_${i}" ${
+      itemsArray[i].checked ? "checked" : ""
+    } />
+                      <textarea disabled>${itemsArray[i].thing}</textarea>
+                      <div class="edit-controller">
+                        <div>
+                          Prioridad
+                          <select id="priority">
+                            <option ${
+                              itemsArray[i].priority === "Alta"
+                                ? "selected"
+                                : ""
+                            }>Alta</option>
+                            <option ${
+                              itemsArray[i].priority === "Media"
+                                ? "selected"
+                                : ""
+                            }>Media</option> 
+                            <option ${
+                              itemsArray[i].priority === "Baja"
+                                ? "selected"
+                                : ""
+                            }>Baja</option> 
+                          </select>
+                        </div>
+                        <div>
+                          Categorías
+                          <select id="category">
+                              <option ${
+                                itemsArray[i].category === "Casa"
+                                  ? "selected"
+                                  : ""
+                              }>Casa</option> 
+                              <option ${
+                                itemsArray[i].category === "Trabajo"
+                                  ? "selected"
+                                  : ""
+                              }>Trabajo</option> 
+                              <option ${
+                                itemsArray[i].category === "Emprendimiento"
+                                  ? "selected"
+                                  : ""
+                              }>Emprendimiento</option> 
+                            </select>
+                        </div>
+                        <i class="fa-solid fa-pen-to-square editBtn"></i>
+                        <i class="fa-solid fa-x deleteBtn"></i>
+                      </div>
+                    </div>
+                    <div class="update-controller">
+                    <button class="saveBtn">Save</button>
+                     <button class="cancelBtn">Cancel</button>
+                    </div>
+                  </div>`;
+  }
+  document.querySelector(".todo-list").innerHTML = items;
+  activateCheckboxListeners();
+  activateDeleteListeners();
+  activateEditListeners();
+  activateSaveListeners();
+  activateCancelListeners();
+}
 
 //El sistema debe permitir EDITAR o MODIFICAR una tarea.
 
@@ -65,4 +262,5 @@ document.querySelector('.new-todo').addEventListener('keyup', (event) => {
 
 //Recordar llamar las funciones displayItems() y displayFooter() para mostrar
 //las tareas en pantalla
-displayFooter()
+displayItems();
+displayFooter();
